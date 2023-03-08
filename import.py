a4a52ac338c21e9a157a94c17bd8c20e54150b41
Rahul M R <rahul.mr@naicoits.com>
2023-03-08 18:17:01 +0530

    dwfaswfgsf

---
+++ import.py
@@ -0,0 +1,14 @@
+import requests
+import json
+response = requests.get("https://api.github.com/repos/rahulmrnaico/Technology/events")
+data = response.text
+parse_json = json.loads(data)
+active_case = parse_json[0]['payload']['head']
+# print(active_case)
+
+import subprocess
+# subprocess.run('git init')
+# subprocess.run('git add .')
+# subprocess.run(f'git commit -m "hi"')
+# subprocess.run('git push')
+subprocess.run('git checkout '+ active_case)
