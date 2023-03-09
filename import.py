63ca7d5be331ef7a9b9571248c455075a65965fc
Rahul M R <rahul.mr@naicoits.com>
2023-03-09 14:21:57 +0530

    dfadfa

--- import.py
+++ import.py
@@ -5,12 +5,11 @@ data = response.text
 parse_json = json.loads(data)
 active_case = parse_json[0]['payload']['head']
 # print(active_case)
+
 import subprocess
 # subprocess.run('git init')
 # subprocess.run('git add .')
 # subprocess.run('git commit -m "hi"')
 # subprocess.run('git push')
 # subprocess.run('git remote set-url --add --push origin https://github.com/rahulmrnaico/Technology')
-subprocess.run('git checkout main')
 subprocess.run('git checkout '+ active_case)
-
